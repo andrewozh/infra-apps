ded22b45b4d2db3bafdb05f16e4c7104bd184a06
Andrew Ozhegov <andrew.ozhegov@gmail.com>
2024-02-10 22:58:20 +0300

    upd init script

--- 1_cluster-init/init.sh
+++ 1_cluster-init/init.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
 
 operator-sdk olm install
+# WARN: manual 'olm' ns edit required
+# pod-security.kubernetes.io/enforce: baseline
+# https://github.com/argoproj-labs/argocd-operator/issues/945
 
 # argocd
 
-kubectl create -n olm -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/catalog_source.yaml
-kubectl create ns argocd
-kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/operator_group.yaml
-kubectl create -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-operator/master/deploy/subscription.yaml
+kubectl apply -k .
+kubectl apply -k ../argocd/init
+
 cat <<EOF | kubectl apply -f -
 apiVersion: argoproj.io/v1alpha1
 kind: ArgoCD
 metadata:
   name: argocd
-    namespace: argocd
-    spec: {}
-EOF
-cat <<EOF | kubectl apply -f -
-apiVersion: argoproj.io/v1alpha1
-kind: Application
-metadata:
-  name: argocd-infra
   namespace: argocd
-spec:
-  project: default
-  source:
-    repoURL: 'https://github.com/andrewozh/infra-apps.git'
-    targetRevision: main
-    path: 1_cluster-init/argocd
-  destination:
-    server: https://kubernetes.default.svc
-    namespace: argocd
-  syncPolicy:
-    automated:
-      prune: true
-      selfHeal: true
 EOF
